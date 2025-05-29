ea23ab20df72cd51f90f601cfd8836166e694b56
EDE MANIDEEP <manideepede9@gmail.com>
2025-05-29 14:59:36 +0530

    Added @FieldDefaults(level = AccessLevel.PRIVATE) for the all the private variables without writing multiple time's before the variable names

--- src/main/java/com/example/Springboot/Learning/model/Employee.java
+++ src/main/java/com/example/Springboot/Learning/model/Employee.java
@@ -4,14 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.experimental.FieldDefaults;
 
 @Entity
 @Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name, department;
-    private Double salary;
+    Long id;
+    String name, department;
+    Double salary;
 }
