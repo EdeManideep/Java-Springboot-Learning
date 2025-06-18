cb88b5dc7f5225414e024a2a7a689ddf68ae16a2
EDE MANIDEEP <manideepede9@gmail.com>
2025-06-18 12:30:19 +0530

    Removed unused imports from the EmployeeService

--- src/main/java/com/example/Springboot/Learning/service/EmployeeService.java
+++ src/main/java/com/example/Springboot/Learning/service/EmployeeService.java
@@ -1,14 +1,11 @@
 package com.example.Springboot.Learning.service;
 
-import java.util.List;
 
 import com.example.Springboot.Learning.dto.EmployeeRegisterDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 import com.example.Springboot.Learning.model.Employee;
 
-@Service
 public interface EmployeeService {
     public Page<Employee> getAll(Pageable pageable);
     public Employee getById(Long id);
